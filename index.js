50a70fd6615e8b302504496758669f7ffb2e8615
Modern Viking <thomascustoms715@gmail.com>
2019-11-26 22:30:47 -0700

    js Pseudocode

---
+++ index.js
@@ -0,0 +1,12 @@
+//length
+var length = [length]
+//upper alpha
+var upper = [A,B,C,D...]
+//lower alpha
+var lower = [a,b,c,d...]
+//numeric
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+//special characters
+var specChar = [specials]
+//generate password
+//changing h5 tag to generated password
