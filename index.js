b073a1deaf17ff99e9db220085ffbfca05942f3f
Modern Viking <thomascustoms715@gmail.com>
2019-12-03 20:45:37 -0700

    set up event listener

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //element grabbers
 var lengthEl = document.getElementById("passLength")
-var specialEL = document.getElementById("special")
-var numbersEL = document.getElementById("numbers")
+var specialEl = document.getElementById("special")
+var numbersEl = document.getElementById("numbers")
 var lowerEl = document.getElementById("lower")
 var upperEl = document.getElementById("upper")
 var submit = document.getElementById("submit")
+var generatedPass = document.getElementById("newPass")
 
 //options arrays
 var upperChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
@@ -15,43 +16,50 @@ var numberSet = [0123456789];
 
 var specChar = ["!@#$%&*"];
 
+var passOptions = [""]
+
 // values for check boxes
 var lowerChecked = lowerEl.checked;
 var upperChecked = upperEl.checked;
 var numbersChecked = numbersEl.checked;
 var specialChecked = specialEl.checked;
 
+//length value
+var length = parseInt(lengthEl.value);
+
+
+
 //event listener
 submit.addEventListener("click", function(){
-    event.preventDefault();
 
-
-
-//length
-var length = parseInt(lengthEl.value);
+    if (lowerChecked){
+        passOptions += lowerChar
+    };
+    if (upperChecked){
+        passOptions += upperChar
+    };
+    if (numbersChecked){
+        passOptions += numberSet
+    };
+    if (specialChecked){
+        passOptions += specChar
+    };
+    
 //if length input is outside 8-128 chars then error appears
 if (length < 8 || length > 128){
     alert ("Password length must be between 8 - 128 characters.")
 };
 
 // if no options are chosen an error appears
-if (!lowerChecked && !upperChecked && !numbersChecked && !symbols) {
+if (!lowerChecked && !upperChecked && !numbersChecked && !specialChecked) {
     alert("Please select at least one option for your password.")
 };
 
-
-//generate password
-var newPass = ("");
-//function genPass() {}
-
-//changing h5 tag to generated password
-//h5.textContent = (newPass);
-
-
-
-//console logs
-
+generatedPass.textContent = Generate(); 
 
 })
 
-//
+//password generator function
+function generate(){
+
+}
