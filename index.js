3ef97f19bf1b9ce013018021c04e6842a09b4c14
Modern Viking <thomascustoms715@gmail.com>
2019-12-03 19:59:58 -0700

    fixing variables syntax

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 //element grabbers
-var lengthEl = document.getElementById(passLength)
-var specialEL = document.getElementById(special)
-var numbersEL = document.getElementById(numbers)
-var lowerEl = document.getElementById(lower)
-var upperEl = document.getElementById(upper)
-var submit = document.getElementById(submit)
+var lengthEl = document.getElementById("passLength")
+var specialEL = document.getElementById("special")
+var numbersEL = document.getElementById("numbers")
+var lowerEl = document.getElementById("lower")
+var upperEl = document.getElementById("upper")
+var submit = document.getElementById("submit")
+
+//options arrays
+var upperChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+
+var numberSet = [0123456789];
+
+var specChar = ["!@#$%&*"];
+
+//event listener
+submit.addEventListener("click", function(){
+    event.preventDefault();
+
+
 
 //length
-var length = (lengthEl.value);
+var length = parseInt(lengthEl.value);
 //if length input is outside 8-128 chars then error appears
 if (length < 8 || length > 128){
     alert ("Password length must be between 8 - 128 characters.")
@@ -35,13 +50,8 @@ var newPass = ("");
 
 
 //console logs
-console.log(length);
 
-//event listener
-submit.addEventListener("click", function(){
-    event.preventDefault();
 
 })
-
 
 //
