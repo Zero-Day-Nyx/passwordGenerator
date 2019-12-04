4f64e7e89e06cce4ae44e78957fe5e941b5900f7
Modern Viking <thomascustoms715@gmail.com>
2019-12-03 20:00:35 -0700

    fixing variables again

--- index.js
+++ index.js
@@ -28,17 +28,6 @@ if (length < 8 || length > 128){
     alert ("Password length must be between 8 - 128 characters.")
 };
 
-//upper alpha
-var upperChar = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-
-//lower alpha
-var lowerChar = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-
-//numeric
-var numberSet = [0,1,2,3,4,5,6,7,8,9];
-
-//special characters
-var specChar = ["!,@,#,$,%,&,*"];
 
 //generate password
 var newPass = ("");
