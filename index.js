81035b64f9865bc464d618e75cd7ec765cbf0249
Modern Viking <thomascustoms715@gmail.com>
2019-12-06 23:04:51 -0700

    added copy to clipboard and generate functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var numbersEl = document.getElementById("numbers")
 var lowerEl = document.getElementById("lower")
 var upperEl = document.getElementById("upper")
 var submit = document.getElementById("submit")
+var copy = document.getElementById("copy")
 var generatedPass = document.getElementById("newPass")
 
 //options arrays
@@ -25,12 +26,12 @@ var numbersChecked = numbersEl.checked;
 var specialChecked = specialEl.checked;
 
 //length value
-var length = parseInt(lengthEl.value);
+var lengthNumber = parseInt(lengthEl.value);
 
 
-
-//event listener
-submit.addEventListener("click", function(){
+//generator event listener
+submit.addEventListener("click", function(event){
+    event.preventDefault();
 
     if (lowerChecked){
         passOptions += lowerChar
@@ -44,9 +45,10 @@ submit.addEventListener("click", function(){
     if (specialChecked){
         passOptions += specChar
     };
-    
+    console.log(passOptions)
+    console.log(lengthNumber)
 //if length input is outside 8-128 chars then error appears
-if (length < 8 || length > 128 || !length){
+if (lengthNumber < 8 || lengthNumber > 128 || !lengthNumber){
     alert ("Password length must be between 8 - 128 characters.")
 };
 
@@ -55,11 +57,37 @@ if (!lowerChecked && !upperChecked && !numbersChecked && !specialChecked) {
     alert("Please select at least one option for your password.")
 };
 
-generatedPass.textContent = Generate(); 
+    generatedPass.textContent = generate(length, passOptions);
 
-})
+});
+
 
 //password generator function
-function generate(){
+function generate(length, passOptions){
 
-}
+    //Iniitalize final password as blank string
+    let password = '';
+    
+    //Append random character from the passwordOptions
+    for (let i = 0; i < length; i++) {
+            password += passOptions.charAt(Math.floor(Math.random() * passOptions.length))
+    };
+    return password;
+};
+
+function copytoClipboard(){
+
+        /* Get the text field */
+        var copyText = document.getElementById("myInput");
+      
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        alert("Copied the text: " + copyText.value);
+        return
+};
