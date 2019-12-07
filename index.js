72b23defe6b660522129d9098b38336ce51139e1
Modern Viking <thomascustoms715@gmail.com>
2019-12-07 15:12:21 -0700

    final debugging

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 //element grabbers
-var lengthEl = document.getElementById("passLength")
-var specialEl = document.getElementById("special")
-var numbersEl = document.getElementById("numbers")
-var lowerEl = document.getElementById("lower")
-var upperEl = document.getElementById("upper")
-var submit = document.getElementById("submit")
-var copy = document.getElementById("copy")
-var generatedPass = document.getElementById("newPass")
+var lengthEl = document.getElementById("passLength");
+var specialEl = document.getElementById("special");
+var numbersEl = document.getElementById("numbers");
+var lowerEl = document.getElementById("lower");
+var upperEl = document.getElementById("upper");
+var submit = document.getElementById("submit");
+var copy = document.getElementById("copy");
+var generatedPass = document.getElementById("newPass");
+var lengthNumber = parseInt(lengthEl.value);
 
 //options arrays
-var upperChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
 
-var numberSet = [0123456789];
+var numberSet = "0123456789";
 
-var specChar = ["!@#$%&*"];
+var specChar = "!@#$%&*";
 
-var passOptions = [""]
 
 // values for check boxes
 var lowerChecked = lowerEl.checked;
@@ -25,13 +25,12 @@ var upperChecked = upperEl.checked;
 var numbersChecked = numbersEl.checked;
 var specialChecked = specialEl.checked;
 
-//length value
-var lengthNumber = parseInt(lengthEl.value);
-
-
 //generator event listener
 submit.addEventListener("click", function(event){
     event.preventDefault();
+    
+    var lengthNumber = parseInt(lengthEl.value);
+    var passOptions = ""
 
     if (lowerChecked){
         passOptions += lowerChar
@@ -45,41 +44,44 @@ submit.addEventListener("click", function(event){
     if (specialChecked){
         passOptions += specChar
     };
-    console.log(passOptions)
-    console.log(lengthNumber)
+console.log(passOptions)
+console.log(lengthNumber)
 //if length input is outside 8-128 chars then error appears
-if (lengthNumber < 8 || lengthNumber > 128 || !lengthNumber){
-    alert ("Password length must be between 8 - 128 characters.")
+    if (lengthNumber < 8 || lengthNumber > 128 || !lengthNumber){
+        alert ("Password length must be between 8 - 128 characters.")
 };
 
 // if no options are chosen an error appears
-if (!lowerChecked && !upperChecked && !numbersChecked && !specialChecked) {
-    alert("Please select at least one option for your password.")
+    if (!lowerChecked && !upperChecked && !numbersChecked && !specialChecked) {
+        alert("Please select at least one option for your password.")
 };
+console.log(generatedPass)
 
-    generatedPass.textContent = generate(length, passOptions);
+    generatedPass.value = generate(lengthNumber, passOptions);
+    
 
+    return; 
 });
 
-
 //password generator function
-function generate(length, passOptions){
+function generate(lengthNumber, passOptions){
 
     //Iniitalize final password as blank string
     let password = '';
     
     //Append random character from the passwordOptions
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < lengthNumber; i++) {
             password += passOptions.charAt(Math.floor(Math.random() * passOptions.length))
     };
     return password;
 };
 
+
 function copytoClipboard(){
 
         /* Get the text field */
-        var copyText = document.getElementById("myInput");
-      
+        var copyText = document.getElementById("newPass");
+    
         /* Select the text field */
         copyText.select();
         copyText.setSelectionRange(0, 99999); /*For mobile devices*/
@@ -89,5 +91,8 @@ function copytoClipboard(){
       
         /* Alert the copied text */
         alert("Copied the text: " + copyText.value);
-        return
+
+        console.log(copyText.value);
+
+        return;
 };
